8aed522b37220092ff6b1bbeb21c8a565ee7a3a2
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-10 14:20:00 +0300

    added value for prompt and logic to it

--- exerciseXPgold/script.js
+++ exerciseXPgold/script.js
@@ -1,20 +1,39 @@
 // guess a hidden word
 
 // Prompt player 1 for a word. The word must have a minimum of 8 letters. Present the word in the console with stars (********).
-let word = prompt('Write a word minimum 8 letters'); 
+let wordPlayer1 = prompt('Write a word minimum 8 letters'); 
+let wordLength = wordPlayer1.length;
+let incorrectGuesses = 0;
 
-if (word.length < 8) {
-  word = prompt("Bro, your word is less then 8 letters, try again");
-}
-let newWord;
-for (let i = 0; i < word.length; i++) {
-  hiddenWord = word.replaceAll(word, "*");
-  console.log(hiddenWord);
+if (wordLength < 8) {
+  wordPlayer1 = prompt("Bro, your word is less then 8 letters, try again");
 }
 
-console.log(newWord)
+let newWord = "*".repeat(wordLength);
+console.log(`The word is ${newWord}`)
+
+let splitWord = wordPlayer1.split('')
+console.log(splitWord)
+// for (let i = 0; i < word.length; i++) {
+//   newWord = word.replaceAll(word, "*");
+//   console.log(newWord);
+// }
 // At this point continuously prompt player 2 for a letter.
+
+let letterPlayer2 = prompt('Try to guess a word, type one letter');
+
+if (letterPlayer2.length > 1) {
+  letterPlayer2 = prompt("Bro, just one letter at once");
+}
 // If the letter is in the word chosen by player 1, display the word in stars again but with the correct letter (*****t**).
+
+for (let i = 0; i < splitWord.length; i++){
+  if (splitWord[i] == letterPlayer2) {
+    console.log(letterPlayer2)
+  } else {
+    incorrectGuesses++;
+  }
+}
 // If the letter appears in the word multiple times, make sure it is seen in all the correct places when showing the stars with the correct guesses (***t**t*).
 // If player 2 guesses incorrectly 10 times display a message in the console saying YOU LOSE.
 // Show a list of all the guesses after each turn. In your code prevent player 2 from guessing the same letter twice.
